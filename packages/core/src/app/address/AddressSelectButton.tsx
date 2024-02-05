61ec94f48e7fffa2992a4c78bfcdf843b86014c0
Nicole Aitchison <nlaitchison@gmail.com>
2024-02-05 13:49:25 -0800

    Rename aria-description to remove console error

--- packages/core/src/app/address/AddressSelectButton.tsx
+++ packages/core/src/app/address/AddressSelectButton.tsx
@@ -3,37 +3,36 @@ import React, { FunctionComponent, useState } from 'react';
 import { preventDefault } from '@bigcommerce/checkout/dom-utils';
 import { TranslatedString, withLanguage, WithLanguageProps } from '@bigcommerce/checkout/locale';
 
-
 import { AddressSelectProps } from './AddressSelect';
 import StaticAddress from './StaticAddress';
 
 type AddressSelectButtonProps = Pick<AddressSelectProps, 'selectedAddress' | 'addresses' | 'type'>;
 
 const AddressSelectButton: FunctionComponent<AddressSelectButtonProps & WithLanguageProps> = ({
-    selectedAddress,
-    language,
-    type
+  selectedAddress,
+  language,
+  type,
 }) => {
-    const [ariaExpanded, setAriaExpanded] = useState(false);
+  const [ariaExpanded, setAriaExpanded] = useState(false);
 
-    return (
-        <a
-            aria-controls="addressDropdown"
-            aria-description={language.translate('address.enter_or_select_address_action')}
-            aria-expanded={ariaExpanded}
-            className="button dropdown-button dropdown-toggle--select"
-            href="#"
-            id="addressToggle"
-            onClick={preventDefault(() => setAriaExpanded(!ariaExpanded))}
-            onBlur={() => setAriaExpanded(false)}
-        >
-            {selectedAddress ? (
-                <StaticAddress address={selectedAddress} type={type} />
-            ) : (
-                <TranslatedString id="address.enter_address_action" />
-            )}
-        </a>
-    );
+  return (
+    <a
+      aria-controls="addressDropdown"
+      aria-expanded={ariaExpanded}
+      aria-label={language.translate('address.enter_or_select_address_action')}
+      className="button dropdown-button dropdown-toggle--select"
+      href="#"
+      id="addressToggle"
+      onBlur={() => setAriaExpanded(false)}
+      onClick={preventDefault(() => setAriaExpanded(!ariaExpanded))}
+    >
+      {selectedAddress ? (
+        <StaticAddress address={selectedAddress} type={type} />
+      ) : (
+        <TranslatedString id="address.enter_address_action" />
+      )}
+    </a>
+  );
 };
 
 export default withLanguage(AddressSelectButton);
