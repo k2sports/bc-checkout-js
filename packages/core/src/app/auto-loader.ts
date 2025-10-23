fbb8d29730aef828c1c73d28b49a0e2617868ddc
Nicole Aitchison <nlaitchison@gmail.com>
2025-10-23 15:48:55 -0700

    Update log version

--- packages/core/src/app/auto-loader.ts
+++ packages/core/src/app/auto-loader.ts
@@ -30,7 +30,7 @@ function isCustomCheckoutWindow(window: Window): window is CustomCheckoutWindow 
     throw new Error('Checkout config is missing.');
   }
 
-  console.log('Manage Shipping Methods v2.0.1-beta.4', window.checkoutConfig);
+  console.log('Manage Shipping Methods v2.1.0', window.checkoutConfig);
 
   const { renderOrderConfirmation, renderCheckout } = await loadFiles();
 
